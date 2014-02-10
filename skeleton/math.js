8a8f4b6a06d80721d5202012f864981ea85c7722
Arigbabuwo A <jalamiaswagger@gmail.com>
2014-02-10 02:23:48 -0800

    helps a lot
    you know

---
+++ skeleton/math.js
@@ -0,0 +1,16 @@
+﻿math = {};
+
+/*math.AoverB = function()
+{
+	this.dom = document.getElementById("math");
+	
+	if (!this.dom || typeof this.dom == "null") {return null;}
+	
+	if (this.dom.getAttribute("math") == "over")
+	{
+		children = this.dom.childNodes;
+		
+		
+	}
+	
+}*/
